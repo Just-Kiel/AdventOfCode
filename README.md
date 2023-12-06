705beeb60e27b0ea65881bdb07148f0eb2fa03ff
Aurore <82760936+Just-Kiel@users.noreply.github.com>
2023-12-06 22:30:59 +0900

    🤶 Day 5 part 1 done

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ Done in Python 3.9.0
 - Day 2: Done with ⭐⭐
 - Day 3: Failed
 - Day 4: Done with ⭐⭐ (within 30 minutes)
-- Day 5: Failed because of memory error (Update : now no memory error but still failed)
+- Day 5: Failed because of memory error (Update : now no memory error but still failed) (Update 2 : Done with ⭐)
 - Day 6: Done with ⭐⭐ (within 20 minutes)
--- day05/day05.py
+++ day05/day05.py
@@ -13,38 +13,10 @@ seeds = [int(x) for x in Lines[0].split(": ")[1].split(" ")]
 # Second pass
 # Just pass seeds and get the result
 
-# instructions = []
-# dictData = {}
-# for index, line in enumerate(Lines[2:]):
-#     index += 1
-#     line = line.strip()
-
-#     if line == "": 
-#         # append dict to instructions
-#         instructions.append(dictData)
-#         dictData = {}
-#         continue
-#     if line[0].isalpha(): continue
-
-#     info = [int(x) for x in line.split(" ")]
-
-#     keys = range(info[1], info[1]+info[2])
-#     values = range(info[0], info[0]+info[2])
-#     dictData.update(dict(zip(keys, values)))
-#     # id = 0
-#     # for key in range(info[1], info[1]+info[2]):
-#     #     dictData[key] = info[0]+id
-#         # id+=1
-
-# instructions.append(dictData)
-
-# for instruction in instructions:
-#     for index, seed in enumerate(seeds):
-#         seeds[index] = instruction[seed] if seed in instruction else seed
-
 # Version 2
 newInstruction = True
 for index, seed in enumerate(seeds):
+    newInstruction = True
     for line in Lines[2:]:
         line = line.strip()
         if line == "": 
@@ -56,19 +28,39 @@ for index, seed in enumerate(seeds):
             info = [int(x) for x in line.split(" ")]
             
             if seeds[index] >= info[1] and seeds[index] < info[1]+info[2]:
-                print(seeds[index], info) if index == 0 else None
                 seeds[index] = info[0]+(seeds[index]-info[1])
                 newInstruction = False
-                break
 
-print(seeds)
 result = min(seeds)
 print("Result : {}".format(result))
-# 212517114 too low
 # 1181555926
-# 312375827
 
 print ("Day {} - Part two !".format(dayNumber))
 result = 0
 
+seeds = []
+infoLine = [int(x) for x in Lines[0].split(": ")[1].split(" ")]
+index = 0
+while index < len(infoLine):
+    seeds.extend(range(infoLine[index], infoLine[index]+infoLine[index+1]+1))
+    index += 2
+    
+newInstruction = True
+for index, seed in enumerate(seeds):
+    newInstruction = True
+    for line in Lines[2:]:
+        line = line.strip()
+        if line == "": 
+            newInstruction = True
+            continue
+        if line[0].isalpha(): continue
+        
+        if newInstruction:
+            info = [int(x) for x in line.split(" ")]
+            
+            if seeds[index] >= info[1] and seeds[index] < info[1]+info[2]:
+                seeds[index] = info[0]+(seeds[index]-info[1])
+                newInstruction = False
+
+result = min(seeds)
 print("Result : {}".format(result))
